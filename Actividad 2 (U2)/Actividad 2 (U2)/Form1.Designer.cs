ae418a846259488f1aaadf99ae4adc4ae21f7b9b
Jonathan Morales Lopez <74636947+JonathanML203@users.noreply.github.com>
2024-03-01 09:55:21 -0600

    Add files via upload

---
+++ Actividad 2 (U2)/Actividad 2 (U2)/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Actividad_2__U2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblActualizacion1 = new MetroFramework.Controls.MetroLabel();
+            this.lblActualizacion2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(23, 86);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Chocolate;
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(1113, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            this.chart2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(1168, 86);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Chocolate;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(300, 300);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // lblActualizacion1
+            // 
+            this.lblActualizacion1.AutoSize = true;
+            this.lblActualizacion1.Location = new System.Drawing.Point(90, 421);
+            this.lblActualizacion1.Name = "lblActualizacion1";
+            this.lblActualizacion1.Size = new System.Drawing.Size(84, 20);
+            this.lblActualizacion1.TabIndex = 2;
+            this.lblActualizacion1.Text = "metroLabel1";
+            // 
+            // lblActualizacion2
+            // 
+            this.lblActualizacion2.AutoSize = true;
+            this.lblActualizacion2.Location = new System.Drawing.Point(1242, 410);
+            this.lblActualizacion2.Name = "lblActualizacion2";
+            this.lblActualizacion2.Size = new System.Drawing.Size(87, 20);
+            this.lblActualizacion2.TabIndex = 3;
+            this.lblActualizacion2.Text = "metroLabel2";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(1217, 33);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(233, 20);
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "(226Z0184) Morales Lopez Jonathan";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(688, 33);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(44, 20);
+            this.metroLabel2.TabIndex = 5;
+            this.metroLabel2.Text = "ITSAV";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1587, 489);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.lblActualizacion2);
+            this.Controls.Add(this.lblActualizacion1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.ForeColor = System.Drawing.Color.Brown;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1587, 489);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1587, 489);
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Brown;
+            this.Text = "Actividad 2 (U2)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private MetroFramework.Controls.MetroLabel lblActualizacion1;
+        private MetroFramework.Controls.MetroLabel lblActualizacion2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+    }
+}
+
---
+++ Actividad 2 (U2)/Actividad 2 (U2)/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Actividad_2__U2_
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        Thread Hilo1;
+        Thread Hilo2;
+
+        public void Grafica1()
+        {
+            while (true)
+            {
+                Series s1 = new Series("Grados");
+                s1.ChartType = SeriesChartType.Column;
+
+                if (chart1.InvokeRequired)
+                {
+                    chart1.Invoke(new MethodInvoker(delegate
+                    {
+                        chart1.Series.Clear();
+                        chart1.Series.Add(s1);
+                    }));
+                }
+                else
+                {
+                    chart1.Series.Clear();
+                    chart1.Series.Add(s1);
+                }
+
+                for (int i = 0; i < 100; i++)
+                {
+                    Random numeroAleatorio = new Random();
+                    int numero = numeroAleatorio.Next(1, 10);
+
+                    if (chart1.InvokeRequired)
+                    {
+                        chart1.Invoke(new MethodInvoker(delegate
+                        {
+                            chart1.Series["Grados"].Points.AddY(numero);
+                            chart1.Series["Grados"].Points[i].AxisLabel = i.ToString();
+                            chart1.Series["Grados"].YValueType = ChartValueType.String;
+                            chart1.Series["Grados"].Points[i].Label = numero.ToString();
+                        }));
+                    }
+                    else
+                    {
+                        chart1.Series["Grados"].Points.AddY(numero);
+                        chart1.Series["Grados"].Points[i].AxisLabel = i.ToString();
+                        chart1.Series["Grados"].YValueType = ChartValueType.String;
+                        chart1.Series["Grados"].Points[i].Label = numero.ToString();
+                    }
+                }
+                if (lblActualizacion1.InvokeRequired)
+                {
+                    lblActualizacion1.Invoke(new MethodInvoker(delegate
+                    {
+                        lblActualizacion1.Text = DateTime.Now.ToString();
+                    }));
+                }
+                else
+                {
+
+                }
+
+                Thread.Sleep(7500);
+            }
+            
+        }
+
+        public void Grafica2()
+        {
+            while (true)
+            {
+                Random rn = new Random();
+                int numeroRandom = rn.Next(1, 100);
+                int x = numeroRandom;
+                int y = 100 - numeroRandom;
+
+                if (chart2.InvokeRequired)
+                {
+                    chart2.Invoke(new MethodInvoker(delegate
+                    {
+                        chart2.Series.Clear();
+                        chart2.BackColor = Color.Transparent;
+                        Series serie1 = new Series
+                        {
+                            Name = "Grados",
+                            IsVisibleInLegend = false,
+                            Color = System.Drawing.Color.Green,
+                            ChartType = SeriesChartType.Pie
+                        };
+
+                        chart2.Series.Add(serie1);
+                        serie1.Points.Add(x);
+                        serie1.Points.Add(y);
+                    }));
+                }
+                else
+                {
+                    chart2.Series.Clear();
+                    chart2.BackColor = Color.Transparent;
+                    Series serie1 = new Series
+                    {
+                        Name = "Grados",
+                        IsVisibleInLegend = false,
+                        Color = System.Drawing.Color.Green,
+                        ChartType = SeriesChartType.Pie
+                    };
+
+                    chart2.Series.Add(serie1);
+                    serie1.Points.Add(x);
+                    serie1.Points.Add(y);
+                }
+
+                if (lblActualizacion2.InvokeRequired)
+                {
+                    lblActualizacion2.Invoke(new MethodInvoker(delegate
+                    {
+                        lblActualizacion2.Text = DateTime.Now.ToString();
+                    }));
+                }
+                else
+                {
+                    lblActualizacion2.Text = DateTime.Now.ToString();
+                }
+
+                Thread.Sleep(7500);
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hilo1 = new Thread(new ThreadStart(Grafica1));
+            Hilo1.Start();
+
+            Hilo2 = new Thread(new ThreadStart(Grafica2));
+            Hilo2.Start();
+        }
+    }
+}
